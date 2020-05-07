00c68b77cad8c8a852a0605dbc2bf9c9d8628953
Hristophorov Roman <hristophorov.roman@gmail.com>
2020-05-07 09:40:37 +0300

    Updates

---
+++ precache-manifest.6ac8e0fa12afb762ee48848381da97a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c247d35e37437dc3e7c8ac7d3044dca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e9ab5194c7007ae588c",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0f958ea5490fd882c1f0",
+    "url": "/static/js/2.14057c01.chunk.js"
+  },
+  {
+    "revision": "a9859ba06bcdc4aa43be74188569b44f",
+    "url": "/static/js/2.14057c01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a7e76d1f2dbfa2ace99",
+    "url": "/static/js/3.cbe8b906.chunk.js"
+  },
+  {
+    "revision": "94c750d93da1e702615e",
+    "url": "/static/js/4.46062b64.chunk.js"
+  },
+  {
+    "revision": "9e9ab5194c7007ae588c",
+    "url": "/static/js/main.d661100e.chunk.js"
+  },
+  {
+    "revision": "f1127d2848580f11bad9",
+    "url": "/static/js/runtime-main.3ad0b693.js"
+  }
+]);
